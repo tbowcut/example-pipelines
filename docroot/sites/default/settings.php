be26794a58d30dd7b30942055b68ab99b7a3ac0a
Cameron Tod <cameron.tod@gmail.com>
2013-05-23 22:57:55 +0100

    Add memcache cache backend.

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -9,7 +9,10 @@
  );
 
 
-
 if (file_exists('/var/www/site-php')) {
   require('/var/www/site-php/camerontod/camerontod-settings.inc');
 }
+$conf['cache_backends'][] = './sites/all/modules/memcache/memcache.inc';
+$conf['cache_default_class'] = 'MemCacheDrupal';
+$conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
+
