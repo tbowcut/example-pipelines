eaf5c4887c75a2c605ac8e54cd7ce4f7822bf8e4
Cameron Tod <cameron.tod@gmail.com>
2013-05-23 21:45:19 +0100

    Updated settings.php with local database connection details.

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -1,4 +1,15 @@
 <?php
+  $databases['default']['default'] = array(
+    'driver' => 'mysql',
+    'database' => 'camerontod',
+    'username' => 'root',
+    'password' => 'root',
+    'host' => 'localhost',
+    'prefix' => '',
+ );
+
+
+
 if (file_exists('/var/www/site-php')) {
   require('/var/www/site-php/camerontod/camerontod-settings.inc');
 }
