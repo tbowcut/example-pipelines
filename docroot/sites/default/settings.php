f6aba2de9008301915843bc7f9b38c51bb95dd89
Cameron Tod <cameron.tod@gmail.com>
2013-05-23 22:59:06 +0100

    Fixed incorrect memcache.inc include path.

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -12,7 +12,7 @@
 if (file_exists('/var/www/site-php')) {
   require('/var/www/site-php/camerontod/camerontod-settings.inc');
 }
-$conf['cache_backends'][] = './sites/all/modules/memcache/memcache.inc';
+$conf['cache_backends'][] = './sites/all/modules/contrib/memcache/memcache.inc';
 $conf['cache_default_class'] = 'MemCacheDrupal';
 $conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
 
