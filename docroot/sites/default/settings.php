5e2717c1c512fd4666eadb8815036cd4d3205bcc
Pavithra Raman <pavithra.raman182@gmail.com>
2017-04-07 15:01:28 -0400

    Adding Install profile to settings.php

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -61,3 +61,4 @@ $config['system.performance']['fast_404']['html'] = '<!DOCTYPE html><html><head>
 if (file_exists(__DIR__ . '/settings.local.php')) {
   include __DIR__ . '/settings.local.php';
 }
+$settings['install_profile'] = 'lightning';
